be591fb120d55ad390aaabddbb724bf41f6e61f2
Thomas Breitner <t.breitner@csl.mpg.de>
2023-11-26 15:40:14 +0100

    Reset some Material for MkDocs styles only for this pagetree

--- mkdocs_pagetree_plugin/js/mkdocs_pagetree_plugin.js
+++ mkdocs_pagetree_plugin/js/mkdocs_pagetree_plugin.js
@@ -4,10 +4,53 @@ document.addEventListener("DOMContentLoaded", function (event) {
     // Plugin CSS
     const style = document.createElement('style');
     style.appendChild(document.createTextNode(`
+    /* Reset some default MkDocs styles */
     .pagetree details {
         margin: 0;
         padding: 0;
     }
+    /* Reset some Material for MkDocs styles */
+    .md-typeset .pagetree details {
+        background-color: transparent;
+        border: none;
+        box-shadow: none;
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+    .md-typeset .pagetree summary {
+        padding-left: 3em;
+        padding-top: 0;
+        padding-bottom: 0;
+        background-color: transparent;
+    }
+    .md-typeset .pagetree summary::after {
+        display: none;
+    }
+    .md-typeset .pagetree summary::before {
+        background-color: currentcolor;
+        content: "";
+        height: 1rem;
+        -webkit-mask-image: var(--md-details-icon);
+        mask-image: var(--md-details-icon);
+        -webkit-mask-position: center;
+        mask-position: center;
+        -webkit-mask-repeat: no-repeat;
+        mask-repeat: no-repeat;
+        -webkit-mask-size: contain;
+        mask-size: contain;
+        position: absolute;
+        top: .1em;
+        left: 1.3em;
+        transform: rotate(0deg);
+        transition: transform .25s;
+        width: 1rem;
+      }
+      .md-typeset .pagetree details[open] > summary::before {
+        transform: rotate(90deg);
+      }
+      .md-typeset .pagetree-container ul li {
+        margin-bottom: 0;
+      }
     `));
 
     const head = document.getElementsByTagName('head')[0];
